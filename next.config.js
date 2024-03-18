1c6a5aa5e045e389f77c5218f1be0625554e7c02
Wilson Cusack <wilson.cusack@coinbase.com>
2024-03-18 14:38:32 -0400

    allow as pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: false,
+});
 
-module.exports = nextConfig;
+module.exports = withPWA({
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.resolve.fallback = { fs: false, net: false, tls: false };
+		return config;
+	},
+});
