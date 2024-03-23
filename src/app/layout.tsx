706dcc0cd4d8f6bb5e05700acae978a3c9d0798d
Wilson Cusack <wilson.cusack@coinbase.com>
2024-03-23 14:09:03 -0400

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,58 @@ import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "SW-PWA";
+const APP_DEFAULT_TITLE = "Smart Wallet PWA";
+const APP_TITLE_TEMPLATE = "%s - Smart Wallet PWA";
+const APP_DESCRIPTION = "An example PWA using Smart Wallet";
+
 export const metadata: Metadata = {
-	title: "Smart Wallet PWA",
-	description: "Smart Wallet PWA demo app",
+	metadataBase: new URL("https://smart-wallet-pwa.vercel.app/"),
+	applicationName: APP_NAME,
+	title: {
+		default: APP_DEFAULT_TITLE,
+		template: APP_TITLE_TEMPLATE,
+	},
+	description: APP_DESCRIPTION,
+	manifest: "/manifest.json",
+	themeColor: "#FFFFFF",
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: "default",
+		title: APP_DEFAULT_TITLE,
+	},
+	formatDetection: {
+		telephone: false,
+	},
+	openGraph: {
+		title: APP_DEFAULT_TITLE,
+		description: APP_DESCRIPTION,
+		url: "https://nextjs.org",
+		siteName: "Next.js",
+		images: [
+			{
+				url: "https://nextjs.org/og.png",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "https://nextjs.org/og-alt.png",
+				width: 1800,
+				height: 1600,
+				alt: "My custom alt",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary",
+		title: {
+			default: APP_DEFAULT_TITLE,
+			template: APP_TITLE_TEMPLATE,
+		},
+		description: APP_DESCRIPTION,
+	},
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
