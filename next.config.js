918d07aac6fc93833017ea5b49a5651daf30adab
Wilson Cusack <wilson.cusack@coinbase.com>
2024-03-23 14:18:11 -0400

    move manifest to public

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-	dest: "public",
-	register: true,
-	skipWaiting: true,
-	disable: false,
-});
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: false
+})
 
 module.exports = withPWA({
-	reactStrictMode: true,
-	webpack: (config) => {
-		config.resolve.fallback = { fs: false, net: false, tls: false };
-		return config;
-	},
-});
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
+})
